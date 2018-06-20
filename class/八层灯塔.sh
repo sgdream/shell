69388769f4b64700d2d9fc342aca20194a3db920
sgdream <35300959+sgdream@users.noreply.github.com>
2018-06-20 15:38:08 +0800

    Update 八层灯塔.sh

--- class/八层灯塔.sh
+++ class/八层灯塔.sh
@@ -3,9 +3,13 @@
 num=765
 a=1
 all=1
+b[0]=1
 for i in {1..7}
 do 
 	a=`echo $a*2|bc`
 	all=`echo $all+$a|bc`
+	b[$i]=$a
+	#echo "${b[$i]}"
 done
-echo `echo $num / $all|bc` 
+#echo `echo $num / $all|bc` #最顶端的数量
+echo `echo ${b[$i]}* $num / $all|bc` #最底端的数量
